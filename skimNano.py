a8b4800caec4b5171ee01b341309459d40a60a8b
huiling110 <huiling2016@outlook.com>
2023-09-07 21:24:10 +0200

    fixed a small bug

--- skimNano.py
+++ skimNano.py
@@ -13,8 +13,10 @@ def main(inputNano = 'root://cmsxrootd.fnal.gov///store/data/Run2023B/Muon0/NANO
 
     # input_file = ROOT.TFile(inputNano, "READ")  #???not sure TFile not working
     # input_tree = input_file.Get("Events")
+    inputNano = 'root://cmsxrootd.fnal.gov/'+ inputNano
     chain = ROOT.TChain("Events")
     chain.AddFile(inputNano)
+    print('input: ', inputNano)
     
     print('entries in old tree: ', chain.GetEntries())
    
@@ -116,10 +118,12 @@ def process_arguments():
     input = '/store/data/Run2023B/Muon0/NANOAOD/PromptNanoAODv11p9_v1-v2/60000/06d25571-df3e-4ceb-9e44-7452add3e004.root'
  
     # Add arguments
-    parser.add_argument('--arg1', type=str, default='root://cmsxrootd.fnal.gov//'+input)
+    # parser.add_argument('--arg1', type=str, default='root://cmsxrootd.fnal.gov//'+input)
+    parser.add_argument('--arg1', type=str, default=input)
     parser.add_argument('--arg2', type=str, default='./output/')
     parser.add_argument('--arg3', type=bool, default=True)
-    parser.add_argument('--arg4', type=bool, default=True)
+    # parser.add_argument('--arg4', type=bool, default=True)
+    parser.add_argument('--arg4', type=bool, default=False)
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -143,8 +147,5 @@ def process_arguments():
 
 if __name__=='__main__':
     args = process_arguments()
-    # if len(argz)>0:
     main(args['arg1'], args['arg2'], args['arg3'], args['arg4'])
     # main(args['arg1'], args['arg2'], False, False)
-    # else:
-    # main()
