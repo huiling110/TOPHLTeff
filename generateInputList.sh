ec7322626ffc983fd9d80f1af3f05bd31ff234ea
cristina.giordano <cristina.giordano@cern.ch>
2025-06-16 14:48:40 +0200

    bash file for running generateInputList.py

---
+++ generateInputList.sh
@@ -0,0 +1,6 @@
+# All 2025
+python3 generateInputList_v2.py --tag Muon2025C
+python3 generateInputList_v2.py --tag EGamma2025C
+# Comparison 2024 run (I)
+# python3 generateInputList_v2.py --tag EGamma2024I
+# python3 generateInputList_v2.py --tag Muon2024I
