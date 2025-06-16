2bbe88f3f7a2a7ad3d28890d1cebdb7a881cb193
cristina.giordano <cristina.giordano@cern.ch>
2025-06-16 16:54:17 +0200

    modified subJobs script + bash file + small generateInputList.sh fix

--- generateInputList.sh
+++ generateInputList.sh
@@ -1,6 +1,6 @@
 # All 2025
-python3 generateInputList_v2.py --tag Muon2025C
-python3 generateInputList_v2.py --tag EGamma2025C
+python3 generateInputList.py --tag Muon2025C
+python3 generateInputList.py --tag EGamma2025C
 # Comparison 2024 run (I)
-# python3 generateInputList_v2.py --tag EGamma2024I
-# python3 generateInputList_v2.py --tag Muon2024I
+# python3 generateInputList.py --tag EGamma2024I
+# python3 generateInputList.py --tag Muon2024I
